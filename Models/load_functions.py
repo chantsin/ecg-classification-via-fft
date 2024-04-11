d56efef85ab193a176e3f6e0e38cef6d3dacba62
Calvin Chan <calvintsingshing.chan@mail.utoronto.ca>
2024-04-11 17:09:56 -0400

    Finished neural network and added RNN

--- Models/load_functions.py
+++ Models/load_functions.py
@@ -2,7 +2,7 @@ import numpy as np
 import wfdb
 
 
-def load_signal(path, metadata):
+def load_signal(path, metadata, freq='low'):
     """Loads whole ECG data set. Provide the path and metadata file.
     
     Examples
@@ -10,8 +10,12 @@ def load_signal(path, metadata):
     >>> X, y = load_signal('../data/physionet.org/files/ptb-xl/1.0.3/', metadata)
     >>> X.shape, y.shape
     ((100, 3), (100,))
+    
     """
-    signals = [wfdb.rdsamp(path + temp_path) for temp_path in metadata['filename_hr']]
+    if freq == 'high':
+        signals = [wfdb.rdsamp(path + temp_path) for temp_path in metadata['filename_hr']]
+    else:
+        signals = [wfdb.rdsamp(path + temp_path) for temp_path in metadata['filename_lr']]
     X_data = np.array([signal for signal, fields in signals])
     y_data = metadata['diagnostic_superclass']
     
@@ -29,6 +33,7 @@ def load_sample_signal(sample_size, path, metadata, random_state=None, freq='low
     >>> X_sample, y_sample = load_sample_signal(1000, path, metadata, random_state=42)
     >>> X_sample.shape, y_sample.shape
     ((1000, 3), (1000,))
+    
     """
     if sample_size <= 5:
         metadata = metadata.groupby('diagnostic_superclass').sample(1, random_state=random_state)
